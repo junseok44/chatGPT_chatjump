1cf981a0a22cfb7c9f9d6666aa7509944dcdcc50
newtonjjang <newtonjjang@naver.com>
2025-05-20 19:27:47 +0900

    fix: url-handler disconnect 메서드 구현안된거 수정

--- src/url-handler.js
+++ src/url-handler.js
@@ -7,31 +7,29 @@ export class UrlHandler {
   }
 
   initialize() {
-    // URL 변경 감지를 위한 MutationObserver 설정
-
-    // console.log("url handler initialized");
-
-    const observer = new MutationObserver(() => {
+    this.handler = new MutationObserver(() => {
       const currentUrl = window.location.href;
 
       if (currentUrl !== this.lastUrl) {
-        // console.log("url changed", currentUrl);
         this.lastUrl = currentUrl;
 
         if (CONFIG.CHAT_PAGE_REGEX.test(currentUrl)) {
-          // console.log("Chat page detected, reinitializing observer");
           this.chatJump.observer.initialize();
         } else {
-          // console.log("Non-chat page detected, disconnecting observer");
           this.chatJump.disconnect();
         }
       }
     });
 
-    // body의 변경 감지
-    observer.observe(document.body, {
+    this.handler.observe(document.body, {
       childList: true,
       subtree: true,
     });
+  }
+
+  disconnect() {
+    if (this.handler) {
+      this.handler.disconnect();
+    }
   }
 }
