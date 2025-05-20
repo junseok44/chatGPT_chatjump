88c3832aaeb00fa784a8ebbf508a67049b1b204c
newtonjjang <newtonjjang@naver.com>
2025-05-20 19:24:21 +0900

    fix: 사용되지 않는 config 삭제

--- src/config.js
+++ src/config.js
@@ -1,9 +1,5 @@
 export const CONFIG = {
   MAX_DOTS: 20,
   TOOLTIP_MAX_LENGTH: 200,
-  UPDATE_DEBOUNCE_MS: 500,
-  URL_CHANGE_DELAY_MS: 1000,
-  SCROLL_OFFSET: 20,
-  URL_PATTERN: /chatgpt\.com\/(?:c|g\/g-[^/]+\/c)\/[^/]+$/,
   CHAT_PAGE_REGEX: /^https:\/\/chatgpt\.com\/(c|g\/[^/]+\/c)\/[^/]+$/,
 };
